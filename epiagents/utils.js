21f33a454e1428fc8b46d45b29801fe9baad258c
Nathan Geffen <nathangeffen@gmail.com>
2022-09-06 06:37:46 +0200

    Changes.

--- epiagents/utils.js
+++ epiagents/utils.js
@@ -39,7 +39,8 @@ function setupAsides()
 setupAsides();
 
 window.MathJax = {
-    tex: {
-        tags: 'ams'
-    }
+  tex: {
+    tags: 'ams',
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  },
 };
