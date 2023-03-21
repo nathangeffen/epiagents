c57fa35f3c457b75812b348200954a7eed25ccc8
Nathan Geffen <nathangeffen@gmail.com>
2023-03-21 19:45:14 +0200

    Updates.

---
+++ procsource.py
@@ -0,0 +1,29 @@
+fi = open("model_example.cc", "r")
+lines = fi.readlines()
+with open("model_example.html", "w") as fo:
+    print("""
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Nathan Geffen" >
+        <meta name="description" content="C++ example of micro model">
+        <meta name="keywords" content="Epidemiology,models,micro models,macro models,agent-based models,simulation,infectious disease">
+        <title>C++ example of micromodel</title>
+    </head>
+    <body>
+        <pre class="prettyprint lang-cc">
+""", file=fo)
+    for line in lines:
+        print(line[:-1], file=fo)
+
+    print("""
+    </pre>
+        <script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script>
+    </body>
+    </head>
+</html>
+""", file=fo)
+fi.close()
